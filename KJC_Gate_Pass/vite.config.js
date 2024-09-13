b70e6add8dacdc283adba0bdc5b9a40686deeae4
AnthonyPint0 <antoniocruze17@gmail.com>
2024-09-13 14:57:57 +0530

    Troubleshooting email

--- KJC_Gate_Pass/vite.config.js
+++ KJC_Gate_Pass/vite.config.js
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // This ensures the build output goes to the "dist" folder
+  },
   server: {
     host: "0.0.0.0", // Listen on all network interfaces
-    port: 5173, // The port you want to use
-    historyApiFallback: true, // Handle client-side routing
+    port: 5173, // The port for dev server
+    strictPort: true, // Prevent Vite from switching ports if 5173 is busy
   },
 });
