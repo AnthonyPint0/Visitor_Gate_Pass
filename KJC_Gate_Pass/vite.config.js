0b4d983945bc961d8bd3bdb8ddd6536631564e85
AnthonyPint0 <antoniocruze17@gmail.com>
2024-09-13 12:40:29 +0530

    fixing SPA

--- KJC_Gate_Pass/vite.config.js
+++ KJC_Gate_Pass/vite.config.js
@@ -4,8 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // Listen on all network interfaces
+    host: "0.0.0.0", // Listen on all network interfaces (useful for network access)
     port: 5173, // The port you want to use
-    historyApiFallback: true, // Handle client-side routing
+    strictPort: true, // Prevents Vite from using a different port if the specified one is occupied
+  },
+  build: {
+    outDir: "dist", // Directory to output build files
   },
 });
